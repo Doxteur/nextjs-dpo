63a818447905446981d68e8bc6d0ccb5cf69dc62
JimmyD <jdoussain@linaia.com>
2022-10-31 16:26:56 +0100

    Ajout Projet

--- pages/components/ProjectCard.js
+++ pages/components/ProjectCard.js
@@ -1,65 +1,39 @@
 import { Card, Grid, Row, Text } from "@nextui-org/react";
 import Image from "next/image";
+import React, { use } from "react";
+import { useEffect } from "react";
 
-export default function ProjectCard() {
-  const list = [
-    {
-      title: "Orange",
-      img: "/images/fruit-1.jpeg",
-      price: "$5.50",
-    },
-    {
-      title: "Tangerine",
-      img: "/images/fruit-2.jpeg",
-      price: "$3.00",
-    },
-    {
-      title: "Cherry",
-      img: "/images/fruit-3.jpeg",
-      price: "$10.00",
-    },
-    {
-      title: "Lemon",
-      img: "/images/fruit-4.jpeg",
-      price: "$5.30",
-    },
-    {
-      title: "Avocado",
-      img: "/images/fruit-5.jpeg",
-      price: "$15.70",
-    },
-    {
-      title: "Lemon 2",
-      img: "/images/fruit-6.jpeg",
-      price: "$8.00",
-    },
-    {
-      title: "Banana",
-      img: "/images/fruit-7.jpeg",
-      price: "$7.50",
-    },
-    {
-      title: "Watermelon",
-      img: "/images/fruit-8.jpeg",
-      price: "$12.20",
-    },
-  ];
+export default function ProjectCard({ filtered,list }) {
+  
+  const [listApp , setListApp] = React.useState(list);
+
+  useEffect(() => {
+    if(filtered == "all"){
+      setListApp(list);
+    }else{
+      setListApp(list.filter((item) => item.type === filtered));
+    }
+  }, [filtered,list]);
+
 
   return (
     <Grid.Container gap={2} justify="flex-start">
-      {list.map((item, index) => (
+      {listApp.map((item, index) => (
         <Grid xs={6} sm={3} key={index}>
           <Card className="scale-x-150" isPressable isHoverable>
             <Card.Body css={{ p: 0 }}>
-              <Card.Image
-                src={"/aquaN2F.png"}
-                alt={item.title}
-              />
+              <Card.Image src={"/aquaN2F.png"} alt={item.title} />
             </Card.Body>
-            <Card.Footer css={{ justifyItems: "flex-start" }} >
+            <Card.Footer css={{ justifyItems: "flex-start" }}>
               <Row wrap="wrap" justify="space-between" align="center">
                 <Text b>{item.title}</Text>
-                <Text css={{ color: "$success", fontWeight: "$semibold", fontSize: "$sm" }}>
+                <Text
+                  css={{
+                    color: "$success",
+                    fontWeight: "$semibold",
+                    fontSize: "$sm",
+                  }}
+                >
                   {item.price}
                 </Text>
               </Row>
--- pages/components/Projects.js
+++ pages/components/Projects.js
@@ -1,19 +1,100 @@
-import React from "react";
+import React, { use, useEffect } from "react";
 import { Box } from "../Box";
 import { Button, Container } from "@nextui-org/react";
 import ProjectCard from "./ProjectCard";
 
 function Projects() {
+  const [filter, setFilter] = React.useState("all");
+  const list = [
+    {
+      title: "Orange",
+      img: "/images/fruit-1.jpeg",
+      price: "$5.50",
+      type: "web",
+    },
+    {
+      title: "Tangerine",
+      img: "/images/fruit-2.jpeg",
+      price: "$3.00",
+      type: "mobile",
+    },
+    {
+      title: "Cherry",
+      img: "/images/fruit-3.jpeg",
+      price: "$10.00",
+      type: "web",
+    },
+    {
+      title: "Lemon",
+      img: "/images/fruit-4.jpeg",
+      price: "$5.30",
+      type: "mobile",
+    },
+    {
+      title: "Avocado",
+      img: "/images/fruit-5.jpeg",
+      price: "$15.70",
+      type: "web",
+    },
+    {
+      title: "Lemon 2",
+      img: "/images/fruit-6.jpeg",
+      price: "$8.00",
+      type: "mobile",
+    },
+    {
+      title: "Banana",
+      img: "/images/fruit-7.jpeg",
+      price: "$7.50",
+    },
+    {
+      title: "Watermelon",
+      img: "/images/fruit-8.jpeg",
+      price: "$12.20",
+    },
+  ];
+  useEffect(() => {
+    setFilter("all");   
+    }, []);
+    
   return (
     <Container md>
       <Container fluid>
-        <div className="flex justify-evenly">
-          <Button>All</Button>
-          <Button>Web</Button>
-          <Button>App</Button>
+        <div className="flex justify-around m-4">
+          <Button
+            auto
+            onClick={() => setFilter("all")}
+            className="m-2"
+            color={filter == "all"?"success":""}
+            shadow={filter == "all"??false}
+            flat={filter == "all"??false}
+
+            >
+            All
+          </Button>
+          <Button
+            auto
+            onClick={() => setFilter("web")}
+            className="m-2"
+            color={filter == "web"?"success":""}
+            shadow={filter == "web"??false}
+            flat={filter == "web"??false}
+            >
+            Web
+          </Button>
+          <Button
+            auto
+            onClick={() => setFilter("mobile")}
+            className="m-2"
+            color={filter == "mobile"?"success":""}
+            shadow={filter == "mobile"??false}
+            flat={filter == "mobile"??false}
+            >
+            Mobile
+          </Button>
         </div>
       </Container>
-      <ProjectCard />
+      <ProjectCard filtered={filter} setFilter={setFilter} list={list}/>
     </Container>
   );
 }
